96ed46dd3aa6b7b1a16330c0f0418004e3b17153
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-07-01 07:06:46 +0530

    c lang

---
+++ C/output.c
@@ -0,0 +1,17 @@
+// Write printf statement to print following on the screen.
+/*5 6
+a=5 and b=6
+a=5 & b=6
+value of a is 5 and value of b is 6
+sum of 5 & 6 is 11
+5+6=1*/
+
+// 5 6
+#include<stdio.h>
+int main()
+{
+    int a=5,b=6;
+    printf("%d %d",a,b)
+}
+
+// a=5 and b=6
