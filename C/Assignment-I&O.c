e1442a69acf901be3ac3bc044c7265dc0b9eada9
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-07-02 13:58:30 +0530

    assignment

---
+++ C/Assignment-I&O.c
@@ -0,0 +1,95 @@
+// 1. Write a program to print "MySirG" on screen in double quotes.
+
+
+// #include <stdio.h>
+
+// int main() {
+//     // Write C++ code here
+//    printf(" \"MySirg\" ");
+//     return 0;
+// }
+
+
+// 2. Write a Program to print '\n' on screen.
+
+
+// #include <stdio.h>
+
+// int main() {
+//     // Write C++ code here
+//    printf("Welcome\nMathew");
+//     return 0;
+// }
+
+
+// 3. Write a Program to print '\r' on screen.
+
+
+// #include <stdio.h>
+
+// int main() {
+//     // Write C++ code here
+//    printf("Welcome\r");
+//    printf("Mathew");
+//     return 0;
+// }
+
+
+// 4. Write a program to calculate sum of two integers.Numbers are taken from user.
+
+
+
+// #include <stdio.h>
+
+// int main() {
+//     int x,y;
+//     // Write C++ code here
+//    printf("Enter Value of 'X'=\n");
+//    scanf("%d",&x);
+//    printf("Enter Value of 'Y'=\n");
+//    scanf("%d",&y);
+//    printf("Sum of %d and %d is =%d",x,y,x+y);
+//     return 0;
+// }
+
+
+//  5. Write a program to calculate the square of a number which is entered by the user.
+
+// #include<stdio.h>
+
+// int main()
+// {
+//     int x,o;
+//     printf("Enter a Number =");
+//     scanf("%d",&x);
+//     o=x*x;
+//     printf("The Square of your given Number is =%d",o);
+// }
+
+// 6. Write a Program to calculate area of rectangle.Take input from the user.
+
+// #include<stdio.h>
+
+// int main()
+// {
+//     int l,b;
+//     printf("Enter Length =\t");
+//    scanf("%d",&l);
+//    printf("Enter Breadth =\t");
+//      scanf("%d",&b);
+//     printf("Area of Rectangle is =%d",l*b);
+// }
+
+
+// 7. Find area of circle . Take Radius of circle from user as input and print the result in below given format.
+
+ #include<stdio.h>
+
+ int main()
+ {
+int p=3.14,a,r;
+printf("Enter radius =\t");
+scanf("%d",&r);
+a=p*(r*r);
+printf("Area of Circle =\t%f",a);
+ }
