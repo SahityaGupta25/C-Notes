94e9a47afe24b12466b094b8c4751dc9cbc25a5e
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-07-11 19:19:38 +0530

    4

--- C/A_4_More_on_Operators.c
+++ C/A_4_More_on_Operators.c
@@ -42,6 +42,7 @@ main()
 */
 
 // 4. Write a program to make last digit stored as a zero in a variable. (Example if - 2345 the make it 2340 )
+/*
 #include<stdio.h>
 int main()
 {
@@ -50,6 +51,23 @@ int main()
     scanf("%d",&x);
     x=(x/10)*10;
     printf("The Last digit is changed to zero - %d",x);
+    return 0;
+}
+*/
+
+// 5.Write a program to input a number from the user and also input a digit . Append a digit in a number and print the resulting number (Example number - 234 digit=7 resulting output - 2347)
+
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    printf("Enter a three digit or four digit number\n");
+    scanf("%d",&x);
+    printf("Enter a Digit\n");
+    scanf("%d",&y);
+    z=(x*10)+y;
+    printf("Resulting output = %d",z);
+    return 0;
 }
 
 // 6. Assume price of 1 USD is INR 84.23 . Write a program to take the amount in INR and convert it into USD.
