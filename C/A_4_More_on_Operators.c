a9ade9b14cdef13b52c97b6a254664b55e6905f4
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-07-11 19:04:22 +0530

    4

--- C/A_4_More_on_Operators.c
+++ C/A_4_More_on_Operators.c
@@ -1,3 +1,4 @@
+/*
 // 1.Write a program to input three digit number and display the sum of the digits.
 #include<stdio.h>
 main()
@@ -11,6 +12,7 @@ main()
     printf("Sum of these three digits is = %d",(a/100)+(a/10%10)+(a%10));
     return 0;
 }
+*/
 
 // 2.Write program to find ASCII code of the character '+'.
 
@@ -21,5 +23,45 @@ main()
     char a='+';
     printf("The ASCII code of '+' is = %d",a);
     return 0; 
+}
+*/
+
+// 3. Write s program to print dize of an int , a float, a char and a double type variable.
+
+/*
+#include<stdio.h>
+main()
+{
+ int x=123,y,z;
+ y=x%10;
+ printf("The last digit of %d is %d",x,y);
+ z=y%y;
+ printf("Now it became %d",y);
+ return 0;    
+}
+*/
+
+// 4. Write a program to make last digit stored as a zero in a variable. (Example if - 2345 the make it 2340 )
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("Enter a three digit or four digit number\n");
+    scanf("%d",&x);
+    x=(x/10)*10;
+    printf("The Last digit is changed to zero - %d",x);
+}
+
+// 6. Assume price of 1 USD is INR 84.23 . Write a program to take the amount in INR and convert it into USD.
+/*
+#include<stdio.h>
+main()
+{
+    float d=84.23,inr,m;
+    printf("Enter amount in ruppes = ");
+    scanf("%f",&inr);
+    m=inr/d;
+    printf("The amount in dollars is =%f",m);
+    return 0;
 }
 */
