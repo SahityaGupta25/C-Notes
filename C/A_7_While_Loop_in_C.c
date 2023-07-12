c11b56f816222ecf3f14fa11b0e161941e862d47
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-07-13 03:54:34 +0530

    7

---
+++ C/A_7_While_Loop_in_C.c
@@ -0,0 +1,170 @@
+// 1.Write a program to print "Radha" 5 times.
+
+/*
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while (i<=5)
+    {
+        printf("Radha\n");
+        i++;
+    }
+    return 0;
+    
+}
+*/
+
+
+// 2.To print first 10 natural Numbers
+
+/*
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while (i<=10)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+    return 0;
+    
+}
+*/
+
+// 3.To print first 10 natural number in reverse order.
+
+/*
+#include<stdio.h>
+int main()
+{
+    int i=10;
+    while (i>=1)
+    {
+        printf("%d\n",i);
+        i--;
+    }
+    return 0;
+    
+}
+*/
+
+// 4.Print 10 odd natural numbers
+
+/*
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while (i<=20)
+    {
+        printf("%d\n",i);
+        i+=2;
+    }
+    return 0;
+    
+}
+*/
+
+// 5.Print 10 odd natural no. in reverse order
+
+/*
+#include<stdio.h>
+int main()
+{
+    int i=19;
+    while (i>=1);
+    {
+        printf("%d\n",i);
+        i-=2;
+    }
+    return 0;
+    
+}
+*/
+
+
+// 6.first even natural no.
+
+
+/*
+#include<stdio.h>
+int main()
+{
+    int i=2;
+    while (i<=20)
+    {
+        printf("%d\n",i);
+        i+=2;
+    }
+    return 0;
+    
+}
+*/
+
+// 7.first even natural no. in reverse order.
+
+/*
+#include<stdio.h>
+int main()
+{
+    int i=20;
+    while (i>=2)
+    {
+        printf("%d\n",i);
+        i-=2;
+    }
+    return 0;
+    
+}
+*/
+
+// 8. Square of 10 natural No.
+
+
+/*
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while (i<=10)
+    {
+        printf("%d\n",i*i);
+        i++;
+    }
+    return 0;
+    
+}
+*/
+
+// 9. Cube of 10 Natural No.
+/*
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while (i<=10)
+    {
+        printf("%d\n",i*i*i);
+        i++;
+    }
+    return 0;
+    
+}
+*/
+
+// 10. Table of 5
+
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while (i<=10)
+    {
+        printf("5x%d = %d\n",i,5*i);
+        i++;
+    }
+    return 0;
+    
+}
