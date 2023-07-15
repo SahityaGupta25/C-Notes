d8ef04a2284e85dd0ff7e5374880b62a1f0bcebf
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-07-15 18:20:56 +0530

    star

--- C/A_11_Star_Pattern_in_C.c
+++ C/A_11_Star_Pattern_in_C.c
@@ -7,19 +7,30 @@
 *****
 
 */
+
+/*
 #include<stdio.h>
 int main()
 {
-    int x,i;
+    int x,i,j;
         printf("Enter a Number = ");
         scanf("%d",&x);
         for ( i = 1; i <= x; i++)
         {
-            for ( i = 1; i <= x ; i++)
+            for ( j = 1; j <= x ; j++)
             {
-                /* code */
+                if (j<=i)
+                {
+                    printf("*");
+                }
+                else{
+                    printf(" ");
+                }
+                
+                
             }
-            
+            printf("\n");
         }
-        
-}
+                return 0; 
+            }
+*/
