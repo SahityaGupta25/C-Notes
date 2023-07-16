145d5c9d258897b17a6c199e79ea05128a456873
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-07-16 20:08:27 +0530

    use of any loop

--- C/A_9_Use_of_any_loop.c
+++ C/A_9_Use_of_any_loop.c
@@ -12,41 +12,50 @@ int main()
 printf("The Factorial of %d is = %d",x,s);
         return 0;
 }
-// 8. Prime no. or not.
+// 8. Write a program to calculate whether a given number is prime or not.
 #include<stdio.h>
 int main()
 {
     int x,i;
         printf("Enter a Number = ");
         scanf("%d",&x);
-        for ( i = 1; i < x; i++)
+        for ( i = 2; i <= x-1; i++)
         {
-           if (x%i!=0){
-            continue;
-           }
-           else if (x%i==0)
-           {
-            printf("Not a Prime Number");
-           }
-           
-           else{
+            if (x%i==0)
+            {
+                
             break;
-           }
+            }
+            
         }
-printf("Prime Number");
+    if (i==x)
+    {
+        printf("%d is a Prime Number",x);
+    }
+    else{
+        printf("%d is not a Prime Number",x);
+    }
+    
+        
+    
 return 0;
 }
 
-// 10. Reverse Number a two-digit number
+// 10. Write a program to Reverse a Given Number 
 
 
 #include<stdio.h>
 int main()
 {
-    int x,y,z;
+    int x,y=0,z;
         printf("Enter a Number = ");
         scanf("%d",&x);
-        y=x/10;
-        z=x%10;
-        printf("%d",z*10+y);
+        while (x!=0)
+        {
+            z=x%10;
+            y=y*10+z;
+            x=x/10;
+        }
+        printf("The Reverse is = %d",y);
+        return 0;
 }
