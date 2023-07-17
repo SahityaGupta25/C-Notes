209a6dc141c36faf07840bc3d613af55a4de94f4
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-07-17 08:58:36 +0530

    pattern

--- C/A_11_Star_Pattern_in_C.c
+++ C/A_11_Star_Pattern_in_C.c
@@ -1,10 +1,75 @@
 /*
-
+1.
 *
 **
 ***
 ****
 *****
+*/
+/*
+#include<stdio.h>
+int main()
+{
+    int x,i,j;
+        printf("Enter a Number = ");
+        scanf("%d",&x);
+        for ( i = 1; i <= x; i++){
+            for ( j = 1; j <= x ; j++){
+                if (j<=i){
+                    printf("*");
+                }
+                else{
+                    printf(" ");
+                }
+            }
+            printf("\n");}
+                return 0; }
+*/
+
+
+/*
+2.
+
+    *
+   **
+  ***
+ ****
+*****
+
+*/
+
+#include<stdio.h>
+int main()
+{
+    int x,i,j;
+        printf("Enter a Number = ");
+        scanf("%d",&x);
+        for ( i = 1; i <= x; i++)
+        {
+            for ( j = 1; j <= x ; j++)
+            {
+                if (j>=6-i)
+                {
+                    printf("*");
+                }
+                else{
+                    printf(" ");
+                }
+                
+                
+            }
+            printf("\n");
+        }
+                return 0; 
+            }
+
+/*
+5.
+1
+12
+123
+1234
+12345
 
 */
 
@@ -21,7 +86,7 @@ int main()
             {
                 if (j<=i)
                 {
-                    printf("*");
+                    printf("%d",j);
                 }
                 else{
                     printf(" ");
@@ -33,4 +98,14 @@ int main()
         }
                 return 0; 
             }
+*/
+
+/*
+6.
+1
+21
+321
+4321
+54321
+
 */
