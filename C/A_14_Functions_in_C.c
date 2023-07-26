24b8e20279c4850b19e047352b02d0fa7ac82db3
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-07-26 21:51:17 +0530

    14

---
+++ C/A_14_Functions_in_C.c
@@ -0,0 +1,53 @@
+// A-14 Functions
+// 1.Write a function to calculate area of Circle.
+
+
+#include <stdio.h>
+float area(float);
+int main() {
+    // Write C code here
+    int r,p=3.14,a;
+    printf("Enter two numbers=\t");
+    scanf("%f",&r);
+    a=area(r);
+    printf("The area of circle is =%f",a);
+    return 0;
+}
+float area(float a){
+    float b;
+    b=a*(b*b);
+    return b;
+}
+
+// 3.Write a function that a function is even or odd . Return 1 if even , otherwise odd (TSRS)
+
+
+
+#include <stdio.h>
+int number(int);
+int main() {
+    // Write C code here
+    int x,y;
+    printf("Enter a number=\t");
+    scanf("%d",&x);
+    y=number(x);
+    if (y==1)
+    {
+        printf("Even Number");
+    }
+    else{
+        printf("ODD");
+    }
+    ;
+    return 0;
+}
+int number(int a){
+    int b;
+    if (a%2==0)
+    {
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
