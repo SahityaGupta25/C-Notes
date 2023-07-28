672c558b0c456fc5bd7f42749c710801bcca5086
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-07-28 20:05:42 +0530

    functions

--- C/A_14_Functions_in_C.c
+++ C/A_14_Functions_in_C.c
@@ -1,22 +1,23 @@
 // A-14 Functions
-// 1.Write a function to calculate area of Circle.
+// 1.Write a function to calculate area of Circle (TSRS).
 
 
 #include <stdio.h>
 float area(float);
 int main() {
     // Write C code here
-    int r,p=3.14,a;
-    printf("Enter two numbers=\t");
+    float r,a;
+    printf("Enter radius=\t");
     scanf("%f",&r);
     a=area(r);
-    printf("The area of circle is =%f",a);
+    printf("The area of circle is = %f",a);
     return 0;
 }
-float area(float a){
-    float b;
-    b=a*(b*b);
-    return b;
+float area(float r){
+    float p=3.14,a;
+    a=p*(r*r);
+    return a;
+
 }
 
 // 3.Write a function that a function is even or odd . Return 1 if even , otherwise odd (TSRS)
@@ -71,4 +72,27 @@ void number(int a){
     printf("%d\n",i);
    }
    
+}
+
+// 6.Write a function to calculate the factorial of a number.
+
+
+#include <stdio.h>
+int fact(int);
+int main() {
+    // Write C code here
+    int x,y;
+    printf("Enter a Number of which you want to calculate Factorial = ");
+    scanf("%d",&x);
+    y=fact(x);
+    printf("The Factorial of %d is =%d",x,y);
+    return 0;
+}
+int fact(int x){
+   int z=1,i;
+   for (int i = x; i >= 1; i--)
+   {
+    z=z*i;
+   }
+return z;   
 }
