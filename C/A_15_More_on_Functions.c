100620c16f5a35f8ddd27b88f259b461d247f04d
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-07-29 11:55:00 +0530

    more o functions

---
+++ C/A_15_More_on_Functions.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int lcm (int,int);
+int main() {
+    // Write C code here
+    int x,f,y;
+    printf("Enter two numbers=\t");
+    scanf("%d %d",&x,&y);
+    f=lcm(x,y);
+    printf("The LCM of both numbers is =%d",f);
+return 0;
+}
+
+int lcm(int x,int y){
+    int i,j,a;
+    for ( i = 2; i <= x<y?x:y ; i++)
+    {
+        if (x%i==0 && y%i==0)
+        {
+            break;
+        }
+        
+    }
+ return i;   
+}
