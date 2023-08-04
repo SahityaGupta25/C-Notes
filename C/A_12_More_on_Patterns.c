249e8e5686e86582ffb63a685408730a47db28ec
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-08-04 19:17:33 +0530

    more

--- C/A_12_More_on_Patterns.c
+++ C/A_12_More_on_Patterns.c
@@ -29,3 +29,38 @@ int main() {
     }
     return 0;
 }   
+
+
+
+/*
+2.
+*********
+ *******
+  *****
+   ***
+    *
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int i,j;
+    for ( i = 1; i <= 5; i++)
+    {
+        for ( j = 1; j <= 9; j++)
+        {
+            if (j>=i && j<=10-i)
+            {
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        
+        }
+        printf("\n");
+    }
+    return 0;
+}  
