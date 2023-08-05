97729f8d84c563c02a17c1ef4a1d314885f3db28
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-08-05 19:55:29 +0530

    recursion

--- C/A_16_Recursion.c
+++ C/A_16_Recursion.c
@@ -1,2 +1,22 @@
-// 2. Write a Recursive function to print first N natural numbers .
+// 1. Write a Recursive function to print first N natural numbers .
 // 2. Write a Recursive function to print first N natural numbers in reverse order.
+
+#include <stdio.h>
+void rev(int);
+int main()
+{
+    int x;
+    printf("Enter UpperLimit = ");
+    rev(x);
+    return 0;
+}
+
+void rev(int x){
+    if (x>=1)
+    {
+        /* code */
+    printf("%d",x);
+    rev(x-1);
+    }
+    
+}
