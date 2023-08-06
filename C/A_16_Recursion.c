a7a545cd34b11aaa1384acae9edb2fb8f14ee36e
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-08-06 15:32:37 +0530

    recursion

--- C/A_16_Recursion.c
+++ C/A_16_Recursion.c
@@ -1,4 +1,26 @@
 // 1. Write a Recursive function to print first N natural numbers .
+
+#include <stdio.h>
+void natural(int);
+int main()
+{
+    int x;
+    printf("Enter value of n = ");
+    scanf("%d",&x);
+    natural(x);
+    return 0;
+}
+
+void natural(int x){
+    if (x>=1)
+    {
+        /* code */
+    natural(x-1);
+    printf("%d",x);
+    }
+    
+}
+
 // 2. Write a Recursive function to print first N natural numbers in reverse order.
 
 #include <stdio.h>
@@ -17,6 +39,34 @@ void rev(int x){
         /* code */
     printf("%d",x);
     rev(x-1);
+    }
+    
+}
+
+// 3. Write a Recursive function to print first N ODD natural numbers .
+
+#include <stdio.h>
+void oddnatural(int);
+int main()
+{
+    int x;
+    printf("Enter value of n = ");
+    scanf("%d",&x);
+    oddnatural(x*2);
+    return 0;
+}
+
+void oddnatural(int x){
+    if (x>=1)
+    {
+    oddnatural(x-1);
+if (x%2!=0)
+{
+    
+    printf(" %d",x);
+}
+
+        
     }
     
 }
