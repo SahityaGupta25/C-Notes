86a98eb739be54997446ca1d5fc9b3969fb77312
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-08-10 19:48:34 +0530

    17-A

--- C/A_17_More_on_Recursion.c
+++ C/A_17_More_on_Recursion.c
@@ -20,4 +20,30 @@ int sumd(int n){
     
 }
 
+// 8. Write a recursive function to print first N fibonacci Series .
 
+
+
+#include <stdio.h>
+int fib(int);
+int main(){
+    int n,i;
+    printf("Enter a Number = ");
+    scanf("%d",&n);
+    for ( i = 1; i <= n; i++)
+    {
+        printf("%d",fib(n));
+    }
+    
+    return 0;
+}
+
+
+
+int fib(int n){ 
+    if (n==1 || n==2)
+        return 1;
+    return (fib(n-1)+fib(n-2));
+
+    
+}
