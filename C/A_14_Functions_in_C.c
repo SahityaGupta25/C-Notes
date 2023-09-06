f05fbdedefd70848c0f51e0c49d429a938ece5ab
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-09-06 11:12:32 +0530

    functions

--- C/A_14_Functions_in_C.c
+++ C/A_14_Functions_in_C.c
@@ -172,38 +172,52 @@ int number(int,int);
 int main() {
     // Write C code here
     int x,n,a;
-    printf("Enter a number");
+    printf("Enter a number= ");
     scanf("%d",&n);
-    printf("Enter a Digit which you want to check");
+    printf("Enter a Digit which you want to check= ");
     scanf("%d",&x);
-    if (number(n,x))
+  if (number(n,x))
     {
         printf("Given number contains a Given digit");
     }
     else 
     {
-       printf("Given number do not contains a Given digit");
+       printf("Given number does not contains a Given digit");
     }
     return 0;
 }
 int number(int n,int x){
-    int b;
-while (n!=0){
-    b=n%10;
-    if (b==x)
-    {
+while (n){
+    if(n%10==x)
         return 1;
-    }
-    else{
-        n=n/10;
-    }
-    return 0;
+    n/=10;
 }
-    
-
+    return 0;
 }
 
 // 10. Write a function to print all prime factors of a given number. For example , if the number is 36 , then your result should be 2,2,3,3 (TSRN)
 
+#include <stdio.h>
+int main() {
+    // Write C code here
+    int n;
+    printf("Enter a number= ");
+    scanf("%d",&n);
+    all_p_f(n);
+    
+}        
 
-        
+void all_p_f(int n){
+        int i=2;
+    while (n>1)
+    {
+        while (n%i==0)
+        {
+            printf("%d ",i);
+            n/=i;
+        }
+        i++;
+    }
+    
+
+}
