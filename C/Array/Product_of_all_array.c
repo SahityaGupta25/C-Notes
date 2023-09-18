714a02e00c6854b2fd2edcf7250f401363dffb56
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2023-09-18 05:41:41 +0530

    array

---
+++ C/Array/Product_of_all_array.c
@@ -0,0 +1,13 @@
+// Product of elements
+#include<stdio.h>
+int main(){
+    int arr[] = {5,4,1,9,6} ,i, s=1;
+    for ( i = 0; i < 5; i++){
+        s*=arr[i];
+    }
+    printf("The Answer is  = %d",s);
+    
+   
+    
+
+}
---
+++ C/Array/sum_of_array.c
@@ -0,0 +1,13 @@
+// Sum of elements
+#include<stdio.h>
+int main(){
+    int arr[] = {5,4,1,9,6} ,i, s=0;
+    for ( i = 0; i < 5; i++){
+        s=s+arr[i];
+    }
+    printf("The Answer is  = %d",s);
+    
+   
+    
+
+}
