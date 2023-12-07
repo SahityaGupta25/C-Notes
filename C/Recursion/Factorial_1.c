8e0714ade793cc7df1a5ceac8e8cdba74ff5a1f0
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2023-12-07 14:10:22 +0530

    factorial

--- C/Recursion/Factorial_1.c
+++ C/Recursion/Factorial_1.c
@@ -1,8 +1,25 @@
 #include<stdio.h>
+int fact(int);
+
+int fact(int n){
+    if (n==1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n*fact(n-1);
+    }
+    
+    
+}
+
 int main(){
-    int n;
+    int n,facct;
     printf("Enter the number of which you want to calculate 'Factorial' = ");
     scanf("%d",&n);
+    facct = fact(n);
+    printf("The Factorial is = %d",facct);
     return 0;
 
 }
