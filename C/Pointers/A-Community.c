3c5e0ea6a455f428e6b4f9a4c0601a01bac730d4
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-01-08 11:23:55 +0530

    pointers

--- C/Pointers/A-Community.c
+++ C/Pointers/A-Community.c
@@ -1,3 +1,29 @@
 // Pointer
 
 // Q.1 Write a function to swap values of two in variables of calling function. (TSRS)
+
+
+#include<stdio.h>
+void swap(int* , int*);
+int main(){
+    int a,b ;
+    printf("Enter the value of 'a'\n");
+    scanf("%d",&a);
+    printf("Enter the value of 'b'\n");
+    scanf("%d",&b);
+    swap(&a,&b);
+    printf("Now the value of a = %d\n",a);
+    printf("Now the value of b = %d",b);
+    
+
+}
+
+void swap(int *x, int *y ){
+int z;
+z=*x;
+*x=*y;
+*y=z;
+
+
+
+}
