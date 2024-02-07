909b2e123f25f5795ff2b4cc217c5d1a80fe991b
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-02-07 10:45:10 +0530

    ex_1

--- C/E_Content/Ex-_1.c
+++ C/E_Content/Ex-_1.c
@@ -1,1 +1,16 @@
 // 1)	Write a C program to find the sum of individual digits of a positive integer.
+
+#include<stdio.h>
+int main(){
+    int sum = 0 , k = 0 , num;
+    printf("Enter a Positive Number whose digits are to be added=\t");
+    scanf("%d",&num);
+    while(num!=0){
+        k=num%10;
+        sum=sum+k;
+        num=num/10;
+
+    }
+    printf("The Total of all digits is = %d",sum);
+    return 0;
+}
