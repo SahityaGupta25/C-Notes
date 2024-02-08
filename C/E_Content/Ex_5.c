13c8a69c0a47489671687544d3dab9dad1f3df47
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-02-08 17:37:28 +0530

    ex5

---
+++ C/E_Content/Ex_5.c
@@ -0,0 +1,15 @@
+// 5. Write a C program to find the factorial of a given integer number using
+// non-recursive functions.
+#include<stdio.h>
+int main (){
+    int num , product = 1, i ;
+    printf("Enter a number = ");
+    scanf("%d",&num);
+    for ( i = 2; i <num+1; i++)
+    {
+        product *= i;
+    }
+    printf("The Factorial of %d is = %d",num,product);
+
+    return 0;
+}
