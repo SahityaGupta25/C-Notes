6d950c528e387e2e4d4ee0988c6a72432b36f0aa
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-02-09 07:27:44 +0530

    string

---
+++ C/Strings/Syntax.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+    int i; 
+    char str[10]={'R','A','D','H','A'};
+    for ( i = 0; i <   10; i++)
+    {
+        printf("%c",str[i]);
+    }
+    
+
+    return 0;
+    
+}
